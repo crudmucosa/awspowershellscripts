8a678b624217d14b77bdff84e2dba492b96b5499
Fenris <crudmucosa@gmail.com>
2017-03-31 22:59:26 -0400

    Script will create a new bucket if it doesn't exist, allow the bucket to act as a website, then set the bucket policy to allow others to see the objects/website

--- createS3Subdomains.ps1
+++ createS3Subdomains.ps1
@@ -1,20 +1,28 @@
+#createS3Subdomains.ps1
+#Create S3 buckets named as subdomains for a given website. Useful for tying said buckets to 
+#AWS Route53 mappings 
 param (
 	[Parameter(Mandatory=$true)][String]$subdomainName,
 	[string]$domainName = 'picturesofus.blah'
 	#[string]$domainName = 'picturesofus.net'
 )
 
-Write-Host Write-S3BucketWebsite -BucketName $subdomainName.$domainName -WebsiteConfiguration_IndexDocumentSuffix index.html
+$bucketName = "$subdomainName.$domainName"
+$policyJSON = '{
+   "Version": "2012-10-17",
+   "Statement": [
+      {
+            "Sid": "PublicReadForGetBucketObjects",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": "s3:GetObject",
+            "Resource": "arn:aws:s3:::' + $bucketName + '/*"
+        }
+    ]
+}'
 
-#{
-#    "Version": "2012-10-17",
-#   "Statement": [
-#      {
-#            "Sid": "PublicReadForGetBucketObjects",
-#            "Effect": "Allow",
-#            "Principal": "*",
-#            "Action": "s3:GetObject",
-#            "Resource": "arn:aws:s3:::picturesofus-aiesha/*"
-#        }
-#    ]
-#}
+New-S3Bucket -BucketName $bucketName -PublicReadOnly
+Write-S3BucketWebsite -BucketName $bucketName -WebsiteConfiguration_IndexDocumentSuffix index.html
+Write-S3BucketPolicy -BucketName $bucketName -Policy $policyJSON
+
+Write-Host Jobs done!
